fc4be11aabb54ee3f58a1bdaae167fd5e035e6e6
Hongyou Xiong <hongyoux@gmail.com>
2018-11-25 11:41:43 -0800

    ShipStats initial version; stub files added for upcoming classes

---
+++ Assets/Scripts/ShipStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipStats {
+    public int health { get; set; }
+    public Vector2 position { get; set; }
+    public float movespeed { get; set; }
+    public int score { get; set; }
+    public Sprite sprite { get; set; }
+}
