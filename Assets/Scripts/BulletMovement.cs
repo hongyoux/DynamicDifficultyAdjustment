f9a7bd3ea09f47ab4bfc1f66cb1d45784ddf383d
Hongyou Xiong <hongyoux@gmail.com>
2018-11-26 01:35:49 -0800

    Created Bullet class added

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Bullet))]
+public class BulletMovement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Bullet b = GetComponent<Bullet>();
+
+        Vector2 newPos = b.stats.position;
+
+        Vector2 change = b.stats.direction * (b.stats.velocity * Time.deltaTime);
+
+        newPos += change;
+
+        b.stats.position = newPos;
+        transform.position = newPos;
+
+        CheckOutOfBounds();
+	}
+
+    void CheckOutOfBounds()
+    {
+        if (transform.position.x < -5 || transform.position.x > 5 ||
+            transform.position.y < -10 || transform.position.y > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BulletStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletStats {
+    public int damage;
+    public Vector2 direction;
+    public Vector2 position;
+    public float velocity;
+    public float acceleration;
+}
