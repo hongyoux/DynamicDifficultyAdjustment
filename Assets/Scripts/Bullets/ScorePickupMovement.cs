2fc90c8c5c2b92481623570850ce0d55ed8d86a9
Hongyou Xiong <hongyoux@gmail.com>
2019-02-17 01:01:04 -0800

    created score pickup. just need to spawn on enemy ship kill now

--- Assets/Scripts/Bullets/ScorePickupMovement.cs
+++ Assets/Scripts/Bullets/ScorePickupMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class ScorePickupMovement : BulletComponent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  float currVelocity;
+  float maxVelocity;
 
-    // Update is called once per frame
-    void Update()
+  private void Start()
+  {
+    currVelocity = stats.velocity;
+    maxVelocity = -2f;
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    Vector2 newPos = stats.position;
+    Vector2 change = stats.direction * (currVelocity * Time.deltaTime);
+
+    newPos += change;
+    stats.position = newPos;
+    transform.position = newPos;
+
+    currVelocity += (stats.acceleration * Time.deltaTime);
+
+    currVelocity = Mathf.Max(maxVelocity, currVelocity); //Cap velocity at -2.
+
+    CheckOutOfBounds();
+  }
+
+  void CheckOutOfBounds()
+  {
+    //Increasing X boundaries on bullets to be more generous around edges
+    if (transform.position.x < -10 || transform.position.x > 10 ||
+        transform.position.y < -10 || transform.position.y > 10)
     {
-        
+      Destroy(gameObject);
     }
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.tag == "PlayerShip")
+    {
+      Gamemaster.Instance.UpdatePlayerScore(10);
+      Destroy(gameObject);
+    }
+  }
 }
